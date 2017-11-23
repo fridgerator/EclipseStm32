57a768583acc481eb004a5f6a6fc3662f4103e8b
zhivko <klemen.zivkovic@gmail.com>
2017-11-23 15:12:23 +0100

    latest

--- Src/main.cpp
+++ Src/main.cpp
@@ -979,9 +979,9 @@ static void MX_ADC1_Init(void) {
 	hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
 	hadc1.Init.Resolution = ADC_RESOLUTION_12B;
 	hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
-	hadc1.Init.ContinuousConvMode = ENABLE;
+	hadc1.Init.ContinuousConvMode = DISABLE;
 	hadc1.Init.DiscontinuousConvMode = DISABLE;
-	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISINGFALLING;
+	hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
 	hadc1.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T1_TRGO;
 	hadc1.Init.DataAlign = ADC_DATAALIGN_LEFT;
 	hadc1.Init.NbrOfConversion = 1;
@@ -1027,9 +1027,9 @@ static void MX_ADC3_Init(void) {
 	hadc3.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
 	hadc3.Init.Resolution = ADC_RESOLUTION_12B;
 	hadc3.Init.ScanConvMode = ADC_SCAN_DISABLE;
-	hadc3.Init.ContinuousConvMode = ENABLE;
+	hadc3.Init.ContinuousConvMode = DISABLE;
 	hadc3.Init.DiscontinuousConvMode = DISABLE;
-	hadc3.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISINGFALLING;
+	hadc3.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
 	hadc3.Init.ExternalTrigConv = ADC_EXTERNALTRIGCONV_T8_TRGO;
 	hadc3.Init.DataAlign = ADC_DATAALIGN_RIGHT;
 	hadc3.Init.NbrOfConversion = 1;
